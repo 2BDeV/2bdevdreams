daab417e97980b35dd8e3e07c611ddd0e05d03da
2BDeV <you@example.combeniiskola@gmail.com>
2025-09-03 15:00:46 +0200

    Bigger 3D logo v0.3

--- src/Logo3d.tsx
+++ src/Logo3d.tsx
@@ -1,17 +1,25 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function Logo3DModel() {
-  const { scene } = useGLTF("/2bdev-logo3d.gbl"); 
-  return <primitive object={scene} scale={2} />; 
+  const { scene } = useGLTF("/2bdev-logo3d.gbl");
+  const ref = useRef<any>();
+  
+  // Automatikus forgatás
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.01;
+    }
+  });
+
+  return <primitive ref={ref} object={scene} scale={2.5} />;
 }
 
 export default function Logo3d() {
   return (
     <div className="w-full h-[500px] flex items-center justify-center">
       <Canvas>
-        {/* Tiltjuk a zoom-ot */}
         <OrbitControls enableZoom={false} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
