0275af2b504fb8c15da00a88f0f68d3b09256cca
2BDeV <you@example.combeniiskola@gmail.com>
2025-09-03 14:55:06 +0200

    Bigger 3D logo

--- src/Logo3d.tsx
+++ src/Logo3d.tsx
@@ -1,22 +1,21 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
-function Model() {
-  const gltf = useGLTF("/2bdev-logo3d.gbl");
-  return <primitive object={gltf.scene} />;
+function Logo3DModel() {
+  const { scene } = useGLTF("/2bdev-logo3d.gbl"); 
+  return <primitive object={scene} scale={1} />;
 }
 
 export default function Logo3d() {
   return (
-    <div className="w-80 h-80">
-      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+    <div className="w-full h-[500px] bg-gray-900 rounded-xl flex items-center justify-center">
+      <Canvas>
+        {/* Tiltjuk a zoom-ot */}
+        <OrbitControls enableZoom={false} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <Suspense fallback={null}>
-          <Model />
-        </Suspense>
-        <OrbitControls enableZoom={true} />
+        <Logo3DModel />
       </Canvas>
     </div>
   );
