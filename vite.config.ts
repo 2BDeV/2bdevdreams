7edfa5609bde4f962d8eaeb3632f2b8424c2378a
2BDeV <beniiskola@gmail.com>
2026-02-09 15:43:41 +0100

    Fix: use React plugin instead of Vue

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    cssCodeSplit: true,
-    minify: 'esbuild',
-    target: 'esnext',
-    esbuild: {
-      drop: ['console', 'debugger'],
-    }
-  }
+  plugins: [react()]
 })
