a5ecccbd42f89a04c14863166166aebc585f3f44
2BDeV <beniiskola@gmail.com>
2026-04-16 16:55:29 +0200

    regular fixing

--- src/Logo3d.tsx
+++ src/Logo3d.tsx
@@ -1,28 +1,33 @@
-import React, { useRef, useMemo } from "react";
+import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF, Environment, ContactShadows } from "@react-three/drei";
 
 function Logo3DModel() {
-  const { scene } = useGLTF("/2bdev-logo3d.gbl");
-  const clonedScene = useMemo(() => scene.clone(true), [scene]);
+  const { scene } = useGLTF("/2bdev-logo3d.glb"); 
   const ref = useRef<any>();
 
   useFrame(() => {
     if (ref.current) {
       ref.current.rotation.y += 0.01;
-      ref.current.rotation.x = 0.2;
     }
   });
 
-  return <primitive object={clonedScene} ref={ref} />;
+  return <primitive object={scene} ref={ref} scale={1.5} />;
 }
 
 export default function Logo3d() {
   return (
-    <Canvas camera={{ position: [0, 0, 3] }} style={{ height: "400px", width: "100%" }}>
-      <ambientLight intensity={0.8} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+    <Canvas camera={{ position: [0, 0, 4] }} style={{ height: "400px", width: "100%" }}>
+      <ambientLight intensity={0.7} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      
       <Logo3DModel />
+      
+      <Environment preset="night" /> 
+      
+      <ContactShadows position={[0, -1.5, 0]} opacity={0.4} scale={10} blur={2} far={4.5} />
+      
       <OrbitControls enableZoom={false} />
     </Canvas>
   );
